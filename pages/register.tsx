666f97cd53b69fb8372485ae7ef73d2cd943baa2
Chairat <mr.chairat_lucky@icloud.com>
2022-05-21 05:20:18 +0700

    simple formik

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Formik, Form, Field, FormikProps } from "formik";
 
 type Props = {};
 
 export default function register({}: Props) {
-  return <div>register</div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{ username: "jj", password: "ll" }}
+        onSubmit={(value) => alert(JSON.stringify(value))}
+      >
+        {({ handleChange, handleSubmit, values }) => (
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="username"
+              onChange={handleChange}
+              value={values.username}
+            />{" "}
+            <br />
+            <input
+              type="text"
+              placeholder="password"
+              onChange={handleChange}
+              value={values.password}
+            />{" "}
+            <br />
+            <button type="submit">Register</button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
 }
