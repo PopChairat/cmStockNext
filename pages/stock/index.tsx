462dcd3a8dabfe445c37e9bcbdfe6777c5586c54
Chairat <mr.chairat_lucky@icloud.com>
2022-05-22 06:31:31 +0700

    edit index stock

--- pages/stock/index.tsx
+++ pages/stock/index.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Layout from "@/components/Layouts/Layout";
+import { resetUsername, userSelector, signUp } from "@/store/slices/userSlice";
+import { store, useAppDispatch } from "@/store/store";
+import { Button } from "@mui/material";
+import { sign } from "crypto";
+import React from "react";
+import { useSelector } from "react-redux";
 
-type Props = {}
+type Props = {};
 
-export default function index({}: Props) {
+export default function Stock({}: Props) {
+  const user = useSelector(userSelector);
+  const dispatch = useAppDispatch();
   return (
-    <div>stock/index</div>
-  )
+    <Layout>
+      <div>index {user.username}</div>
+      <button onClick={() => dispatch(resetUsername({ data: "Hello" }))}>
+        RESET
+      </button>
+      <button
+        onClick={() => dispatch(signUp({ username: "admin", password: "222" }))}
+      >
+        Sign up
+      </button>
+    </Layout>
+  );
 }
