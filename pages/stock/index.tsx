9d0a16101ccbf358e47d9786fc324a76ed925251
Chairat <mr.chairat_lucky@icloud.com>
2022-05-22 20:20:48 +0700

    Test data grid

--- pages/stock/index.tsx
+++ pages/stock/index.tsx
@@ -2,27 +2,75 @@ import Layout from "@/components/Layouts/Layout";
 import withAuth from "@/components/withAuth";
 import { resetUsername, userSelector, signUp } from "@/store/slices/userSlice";
 import { store, useAppDispatch } from "@/store/store";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { sign } from "crypto";
 import React from "react";
 import { useSelector } from "react-redux";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
 type Props = {};
 
 const Stock = ({}: Props) => {
   const user = useSelector(userSelector);
   const dispatch = useAppDispatch();
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (params: GridValueGetterParams) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
   return (
     <Layout>
-      <div>index {user.username}</div>
-      <button onClick={() => dispatch(resetUsername({ data: "Hello" }))}>
-        RESET
-      </button>
-      <button
-        onClick={() => dispatch(signUp({ username: "admin", password: "222" }))}
-      >
-        Sign up
-      </button>
+      <Box>Stock</Box>
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          disableSelectionOnClick
+        />
+      </div>
     </Layout>
   );
 };
