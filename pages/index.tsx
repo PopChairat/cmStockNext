e0f9c57e00a2fb6651f9dcf66f84bebabb662159
Chairat <mr.chairat_lucky@icloud.com>
2022-05-22 15:44:08 +0700

    RUN Prod =>delete .NEXT => yarn build => yarn start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,10 @@
-import Layout from "@/components/Layouts/Layout";
-import { resetUsername, userSelector, signUp } from "@/store/slices/userSlice";
-import { store, useAppDispatch } from "@/store/store";
-import { Button } from "@mui/material";
-import { sign } from "crypto";
+import withAuth from "@/components/withAuth";
 import React from "react";
-import { useSelector } from "react-redux";
 
 type Props = {};
 
-export default function Index({}: Props) {
-  const user = useSelector(userSelector);
-  const dispatch = useAppDispatch();
-  return (
-    <Layout>
-      <div>index {user.username}</div>
-      <button onClick={() => dispatch(resetUsername({ data: "Hello" }))}>
-        RESET
-      </button>
-      <button
-        onClick={() => dispatch(signUp({ username: "admin", password: "222" }))}
-      >
-        Sign up
-      </button>
-    </Layout>
-  );
-}
+const Index = ({}: Props) => {
+  return <></>;
+};
+
+export default withAuth(Index);
